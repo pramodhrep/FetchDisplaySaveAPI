d39c3e588e15e977e0d677b01dca6708ce55461f
Pramod Repaka <200411798@student.georgianc.on.ca>
2019-07-25 18:59:28 -0400

    get and display the API

--- FetchDispSaveAPI/MainPage.xaml.cs
+++ FetchDispSaveAPI/MainPage.xaml.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using FetchDispSaveAPI.Models;
+
 
 namespace FetchDispSaveAPI
 {
@@ -13,9 +17,22 @@ namespace FetchDispSaveAPI
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        //private const string url = "https://jsonplaceholder.typicode.com/posts";
+        private const string url = "https://jsonplaceholder.typicode.com/photos";
+        private HttpClient _Client = new HttpClient();
         public MainPage()
         {
             InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            //Sends a GET request to the specified Uri and returns the response body as a string in an asynchronous operation
+            var content = await _Client.GetStringAsync(url);
+            //Deserializes or converts JSON String into a collection of Post
+            List<Post> posts = JsonConvert.DeserializeObject<List<Post>>(content);
+            //Assigning the ObservableCollection to MyListView in the XAML of this file
+            Post_List.ItemsSource = posts;
+            base.OnAppearing();
         }
     }
 }
---
+++ FetchDispSaveAPI/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace FetchDispSaveAPI.Models
+{
+    public class Post
+    {
+        /*
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; } */
+
+        public int albumId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public string thumbnailUrl { get; set; }
+
+        //empty contructor to use if no data is available to import
+        public Post()
+        {
+        }
+    }
+}
